d6048615c3b6d24067cf0fbb31c18b1411ce4408
Ana Sainz de Murieta <66472224+anasdm@users.noreply.github.com>
2024-09-05 15:53:53 +0100

    Create galaxy_galaxy_lens.py

---
+++ bin/galaxy_galaxy_lens.py
@@ -0,0 +1,56 @@
+# Load necessary libraries
+import numpy as np
+import pandas as pd
+from FastLensSim import FastLensSim
+from tqdm import trange
+import random
+from astropy.cosmology import Planck18
+
+# Load DataFrame
+df = pd.read_csv('../outputs/SNIa/SNIa_GalaxyGalaxyPop.csv')
+
+#Include angular size in arcsec
+df['rs'] = df['R_s']* Planck18.arcsec_per_kpc_comoving(np.array(df['z_host'])).value
+
+# Initialize arrays for computed results
+magnification_gal = np.zeros(len(df))
+x = np.zeros(len(df))
+y = np.zeros(len(df))
+
+# Initialize FastLensSim instance outside the loop
+S = FastLensSim('ideal')
+S.bfac = 2
+S.rfac = 2
+
+for i in trange(len(df)):
+    bs = df['R_Ein'][i]
+    ml = {'g_SDSS': df['mag_g_SDSS'][i], 'r_SDSS': df['mag_r_SDSS'][i], 'i_SDSS': df['mag_i_SDSS'][i]}
+    rl = {'g_SDSS': df['rl'][i], 'r_SDSS': df['rl'][i], 'i_SDSS': df['rl'][i]}
+    ql = df['ql'][i]
+    ms = {'g_SDSS': df['mag_g_host'][i], 'r_SDSS': df['mag_r_host'][i], 'i_SDSS': df['mag_i_host'][i]}
+    qs = (1 - df['e'][i]) / (1 + df['e'][i])
+    ps = df['a_rot'][i]
+    rs = df['rs'][i]
+
+    xs = df['R_Ein'][i] * df['RA_SN'][i] + (random.choice([1, -1])) * df['ra_off'][i]
+    ys = df['R_Ein'][i] * df['DEC_SN'][i] + (random.choice([1, -1])) * df['dec_off'][i]
+    
+    x[i] = xs
+    y[i] = ys
+    
+    S.setLensPars(ml, rl, ql)
+    S.setSourcePars(bs, ms, xs, ys, qs, ps, rs, 1)
+    S.makeLens()
+
+    try:
+        magnification_gal[i] = S.magnification[1]
+    except:
+        magnification_gal[i] = 0
+
+# Assign computed values back to DataFrame
+df['x_gal'] = x
+df['y_gal'] = y
+df['Galaxy Magnification'] = magnification_gal
+
+# Save updated DataFrame
+df.to_csv('../outputs/SNIa/SNIa_GalaxyGalaxyPop_ideal.csv', index=False)
